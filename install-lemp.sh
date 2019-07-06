401fb9092c2b6f7b922946b694351801db35343f
Hunter <thecryptohunter@protonmail.com>
2019-07-06 09:28:32 +0100

    Update config

--- install-lemp.sh
+++ install-lemp.sh
@@ -255,7 +255,8 @@ fi
 #php-xml php-imagick php7.0-zip php7.0-bcmath php-soap \
 #php7.0-intl php7.0-readline
 
-#apt-get install -y php-sqlite3 php-memcached php7.0-mcrypt php-imagick php7.0-bcmath php7.0-intl php7.0-readline
+#apt-get install -y php-sqlite3 
+#php-memcached php7.0-mcrypt php-imagick php7.0-bcmath php7.0-intl php7.0-readline
 
 sudo apt install php7.3-fpm php7.3-common php7.3-mysql php7.3-xml \
 php7.3-xmlrpc php7.3-curl php7.3-gd \
@@ -269,12 +270,12 @@ mv composer.phar /usr/local/bin/composer
 
 # Misc. PHP CLI Configuration
 
-sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.0/cli/php.ini
-sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.0/cli/php.ini
-sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.0/cli/php.ini
-sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.0/cli/php.ini
+sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.3/cli/php.ini
+sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.3/cli/php.ini
+sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.3/cli/php.ini
+sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.3/cli/php.ini
 
-##TODO: update /etc/php/7.0/cli/php.ini >> remove the ; from intl
+##TODO: update /etc/php/7.3/cli/php.ini >> remove the ; from intl
 
 # Configure Sessions Directory Permissions
 
@@ -300,31 +301,31 @@ service nginx restart
 
 # Tweak Some PHP-FPM Settings
 
-sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.0/fpm/php.ini
-sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.0/fpm/php.ini
-sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.0/fpm/php.ini
-sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.0/fpm/php.ini
-sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.0/fpm/php.ini
-sed -i "s/short_open_tag.*/short_open_tag = On/" /etc/php/7.0/fpm/php.ini
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.3/fpm/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.3/fpm/php.ini
+sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.3/fpm/php.ini
+sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.3/fpm/php.ini
+sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.3/fpm/php.ini
+sed -i "s/short_open_tag.*/short_open_tag = On/" /etc/php/7.3/fpm/php.ini
 
 # Setup Session Save Path
 
-sed -i "s/\;session.save_path = .*/session.save_path = \"\/var\/lib\/php5\/sessions\"/" /etc/php/7.0/fpm/php.ini
-sed -i "s/php5\/sessions/php\/sessions/" /etc/php/7.0/fpm/php.ini
+sed -i "s/\;session.save_path = .*/session.save_path = \"\/var\/lib\/php5\/sessions\"/" /etc/php/7.3/fpm/php.ini
+sed -i "s/php5\/sessions/php\/sessions/" /etc/php/7.3/fpm/php.ini
 
 # Configure Nginx & PHP-FPM To Run As User
 
 sed -i "s/user www-data;/user $USER;/" /etc/nginx/nginx.conf
 sed -i "s/# server_names_hash_bucket_size.*/server_names_hash_bucket_size 64;/" /etc/nginx/nginx.conf
-sed -i "s/^user = www-data/user = $USER/" /etc/php/7.0/fpm/pool.d/www.conf
-sed -i "s/^group = www-data/group = $USER/" /etc/php/7.0/fpm/pool.d/www.conf
-sed -i "s/;listen\.owner.*/listen.owner = $USER/" /etc/php/7.0/fpm/pool.d/www.conf
-sed -i "s/;listen\.group.*/listen.group = $USER/" /etc/php/7.0/fpm/pool.d/www.conf
-sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.0/fpm/pool.d/www.conf
+sed -i "s/^user = www-data/user = $USER/" /etc/php/7.3/fpm/pool.d/www.conf
+sed -i "s/^group = www-data/group = $USER/" /etc/php/7.3/fpm/pool.d/www.conf
+sed -i "s/;listen\.owner.*/listen.owner = $USER/" /etc/php/7.3/fpm/pool.d/www.conf
+sed -i "s/;listen\.group.*/listen.group = $USER/" /etc/php/7.3/fpm/pool.d/www.conf
+sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.3/fpm/pool.d/www.conf
 
 # Configure A Few More Server Things
 
-sed -i "s/;request_terminate_timeout.*/request_terminate_timeout = 60/" /etc/php/7.0/fpm/pool.d/www.conf
+sed -i "s/;request_terminate_timeout.*/request_terminate_timeout = 60/" /etc/php/7.3/fpm/pool.d/www.conf
 sed -i "s/worker_processes.*/worker_processes auto;/" /etc/nginx/nginx.conf
 sed -i "s/# multi_accept.*/multi_accept on;/" /etc/nginx/nginx.conf
 
