d171febfd46df9deff161340c59b1eb0bcb639bd
Hunter <thecryptohunter@protonmail.com>
2019-10-01 17:55:17 +0100

    Updates

--- reinstall-web.sh
+++ reinstall-web.sh
@@ -198,7 +198,8 @@ chown ${USER}:www-data /home/${USER}/${SERVER_NAME} -R
 chmod g+rw /home/${USER}/${SERVER_NAME} -R
 chmod g+s /home/${USER}/${SERVER_NAME} -R
 cd /home/${USER}/${SERVER_NAME}
-php /usr/local/bin/composer require btcpayserver/btcpayserver-php-client
+#php /usr/local/bin/composer require trustaking/btcpayserver-php-client
+php /usr/local/bin/composer require trustaking/btcpayserver-php-client:dev-master
 
 ## Inject apiport & ticker into /include/config.php
 sed -i "s/^\(\$ticker='\).*/\1$fork';/" /home/${USER}/${SERVER_NAME}/include/config.php
