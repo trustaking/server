2ef7ee7accdce823665f5fc35d0efe0e72f35115
Hunter <thecryptohunter@protonmail.com>
2019-10-19 10:27:12 +0100

    Update

--- reinstall-web.sh
+++ reinstall-web.sh
@@ -64,7 +64,7 @@ if [[ "$net" =~ ^([tT])+$ ]]; then
             ;;
         obsidian)
             apiport="47221" # "47221" <Main Obsidian
-            printf -v apiver "%q" "&Segwit=true";
+            printf -v apiver "%q" "\"&Segwit=true\"";
             ;;
          *)
            echo "$fork has not been configured."
@@ -90,7 +90,7 @@ else
             ;;
         obsidian)
             apiport="47221";
-            printf -v apiver "%q" "&Segwit=true";
+            printf -v apiver "%q" "\"&Segwit=true\"";
             ;;
          *)
             echo "$fork has not been configured."
