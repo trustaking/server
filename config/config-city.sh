c62fe1acf3bf6aa286e5a61496fe4893947ee31a
Hunter <thecryptohunter@protonmail.com>
2021-01-15 00:24:50 +0000

    update city

--- config/config-city.sh
+++ config/config-city.sh
@@ -21,9 +21,9 @@ function setGeneralVars() {
 #COINRUNCMD="dotnet ./City.Chain.dll ${NETWORK} -agentprefix=trustaking -datadir=/home/${NODE_USER}/.${FORK}chain -maxblkmem=2 -txindex=1 \${stakeparams} \${rpcparams}"
 #COINGITHUB=https://github.com/CityChainFoundation/city-chain.git
 #COINDSRC=/home/${NODE_USER}/code/src/City.Chain
-COINRUNCMD="dotnet ./City.Node.dll ${NETWORK} -agentprefix=trustaking -datadir=/home/${NODE_USER}/.blockcore -maxblkmem=2 -minimumsplitcoinvalue=15000000000 -enablecoinstakesplitting=1 \${stakeparams} \${rpcparams}"
+COINRUNCMD="dotnet ./Blockcore.Node.dll -chain CITY ${NETWORK} -agentprefix=trustaking -datadir=/home/${NODE_USER}/.blockcore -maxblkmem=2 -minimumsplitcoinvalue=15000000000 -enablecoinstakesplitting=1 \${stakeparams} \${rpcparams}"
 COINGITHUB=https://github.com/block-core/blockcore.git
-COINDSRC=/home/${NODE_USER}/code/src/Networks/Blockcore.Networks.City/
+COINDSRC=/home/${NODE_USER}/code/src/Node/Blockcore.Node/
 CONF=release
 COINDAEMON=${FORK}d
 COINCONFIG=${FORK}.conf
